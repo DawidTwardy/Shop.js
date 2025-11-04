a65c1b32f4eebc451b896973bba8967abffb20ea
DawidTwardy <dawidtwardy21@gmail.com>
2025-11-04 13:59:35 +0100

    Initial commit projektu Shop.js

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,5 +1,5 @@
-const { validationResult } = require('express-validator');
 const Product = require('../models/product');
+const fileHelper = require('../util/file');
 
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
@@ -17,8 +17,6 @@ exports.postAddProduct = (req, res, next) => {
   const image = req.file;
   const price = req.body.price;
   const description = req.body.description;
-  const errors = validationResult(req);
-
   if (!image) {
     return res.status(422).render('admin/edit-product', {
       pageTitle: 'Add Product',
@@ -28,41 +26,21 @@ exports.postAddProduct = (req, res, next) => {
       product: {
         title: title,
         price: price,
-        description: description,
-        imageUrl: ''
+        description: description
       },
-      errorMessage: 'Attached file is not a valid image (only .png, .jpg, .jpeg allowed).',
+      errorMessage: 'Attached file is not an image.',
       validationErrors: []
     });
   }
-
-  if (!errors.isEmpty()) {
-    return res.status(422).render('admin/edit-product', {
-      pageTitle: 'Add Product',
-      path: '/admin/add-product',
-      editing: false,
-      hasError: true,
-      product: {
-        title: title,
-        price: price,
-        description: description,
-        imageUrl: ''
-      },
-      errorMessage: errors.array()[0].msg,
-      validationErrors: errors.array()
-    });
-  }
-  
   const imageUrl = image.path;
 
   const product = new Product({
     title: title,
     price: price,
     description: description,
-    imageUrl: imageUrl, 
+    imageUrl: imageUrl,
     userId: req.user
   });
-
   product
     .save()
     .then(result => {
@@ -106,29 +84,9 @@ exports.getEditProduct = (req, res, next) => {
 exports.postEditProduct = (req, res, next) => {
   const prodId = req.body.productId;
   const updatedTitle = req.body.title;
-  const image = req.file;
   const updatedPrice = req.body.price;
+  const image = req.file;
   const updatedDesc = req.body.description;
-
-  const errors = validationResult(req);
-  
-  if (!errors.isEmpty()) {
-    return res.status(422).render('admin/edit-product', {
-      pageTitle: 'Edit Product',
-      path: '/admin/edit-product',
-      editing: true,
-      hasError: true,
-      product: {
-        title: updatedTitle,
-        imageUrl: req.body.oldImageUrl, 
-        price: updatedPrice,
-        description: updatedDesc,
-        _id: prodId
-      },
-      errorMessage: errors.array()[0].msg,
-      validationErrors: errors.array()
-    });
-  }
 
   Product.findById(prodId)
     .then(product => {
@@ -138,11 +96,10 @@ exports.postEditProduct = (req, res, next) => {
       product.title = updatedTitle;
       product.price = updatedPrice;
       product.description = updatedDesc;
-      
       if (image) {
+        fileHelper.deleteFile(product.imageUrl);
         product.imageUrl = image.path;
       }
-      
       return product.save().then(result => {
         res.redirect('/admin/products');
       });
@@ -155,7 +112,9 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.find({ userId: req.user._id })
+  Product.find({
+      userId: req.user._id
+    })
     .then(products => {
       res.render('admin/products', {
         prods: products,
@@ -170,21 +129,29 @@ exports.getProducts = (req, res, next) => {
     });
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
-  Product.findById(prodId)
+exports.deleteProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  
+  Product.findOne({ _id: prodId, userId: req.user._id })
     .then(product => {
       if (!product) {
-        return next(new Error('Product not found.'));
+        // Jeśli produkt nie istnieje LUB użytkownik nie jest jego właścicielem, zwracamy 404/403
+        return res.status(404).json({ message: 'Product not found or not authorized.' });
       }
+
+      // Usuwamy plik (bezpieczna, nieblokująca operacja)
+      fileHelper.deleteFile(product.imageUrl);
+
+      // Zwracamy Promise dla operacji usunięcia z bazy
       return Product.deleteOne({ _id: prodId, userId: req.user._id });
     })
     .then(() => {
-      res.redirect('/admin/products');
+      // Jeśli usunięcie z bazy się powiodło
+      res.status(200).json({ message: 'Success!' });
     })
     .catch(err => {
-      const error = new Error(err);
-      error.httpStatusCode = 500;
-      return next(error);
+      // Obsługa błędu Mongoose lub innego błędu w łańcuchu
+      console.log(err);
+      res.status(500).json({ message: 'Deleting product failed.' });
     });
 };
--- util/file.js
+++ util/file.js
@@ -4,33 +4,35 @@ const multer = require("multer");
 const deleteFile = (filePath) => {
     fs.unlink(filePath, (err) => {
         if (err) {
-            throw (err);
+            console.log(err);
+            // Kluczowa zmiana: nie rzucamy błędu (throw), tylko go logujemy.
+            // Pozwala to na kontynuowanie głównego procesu usuwania rekordu z bazy.
         }
     });
 }
 exports.deleteFile = deleteFile;
 
 const fileStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "images");
-  },
-  filename: (req, file, cb) => {
-    let name = new Date().toISOString() + "-" + file.originalname;
-    name = name.replace(/:/g, "_");
-    cb(null, name);
-  },
+    destination: (req, file, cb) => {
+        cb(null, "images");
+    },
+    filename: (req, file, cb) => {
+        let name = new Date().toISOString() + "-" + file.originalname;
+        name = name.replace(/:/g, "_");
+        cb(null, name);
+    },
 });
 exports.fileStorage = fileStorage;
 
 const fileFilter = (req, file, cb) => {
-  if (
-    file.mimetype === "image/png" ||
-    file.mimetype === "image/jpg" ||
-    file.mimetype === "image/jpeg"
-  ) {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
+    if (
+        file.mimetype === "image/png" ||
+        file.mimetype === "image/jpg" ||
+        file.mimetype === "image/jpeg"
+    ) {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
 };
 exports.fileFilter = fileFilter;
